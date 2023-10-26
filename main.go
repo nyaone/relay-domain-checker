eb43f412ebc67b9556d2793b81fecf65a58827c9
Nya Candy <dev@candinya.com>
2023-10-26 12:36:21 +0800

    refractor: print full nodeinfo & result json

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -8,20 +9,32 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
+)
+
+const (
+	RelayListDomainsFileName = "domains.txt"
+	ResultSaveFileName       = "domains.json"
 )
 
 func main() {
-	domains, err := os.ReadFile("domains.txt")
+	domains, err := os.ReadFile(RelayListDomainsFileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fileLines := strings.Split(strings.Replace(string(domains), "\r", "", -1), "\n")
+	fileLines := strings.Split(strings.ReplaceAll(string(domains), "\r", ""), "\n")
 
-	var unresolvedDomains []string
-	var notFunctioningDomains []string
-	var wrongcodeDomains []string
-	var validDomains []string
+	totalDomainCounter := 0
+
+	var unresolvedList []string
+	var notFunctioningList []string
+	wrongCodeList := make(map[string]int) // domain -> http code (unexpected)
+
+	var misformattedNodeInfoListList []string
+	var noAvailableNodeInfoSchemaList []string
+	var misformattedNodeInfoSchemaList []string
+	validList := make(map[string]NodeInfoSchema)
 
 	var wg sync.WaitGroup
 
@@ -31,6 +44,8 @@ func main() {
 			// Is domain
 			wg.Add(1)
 
+			totalDomainCounter++
+
 			domain := strings.Replace(line, "[*] ", "", 1)
 
 			go func() {
@@ -39,8 +54,8 @@ func main() {
 				// Try resolve
 				_, err := net.LookupIP(strings.Split(domain, ":")[0])
 				if err != nil {
-					unresolvedDomains = append(unresolvedDomains, domain)
-					log.Printf("[%s] is [UNRESOLVED].", domain)
+					unresolvedList = append(unresolvedList, domain)
+					log.Printf("[%s] is unresolved.", domain)
 					return
 				}
 
@@ -48,18 +63,60 @@ func main() {
 				wellknownNodeinfo := fmt.Sprintf("https://%s/.well-known/nodeinfo", domain)
 				resp, err := http.Get(wellknownNodeinfo)
 				if err != nil {
-					notFunctioningDomains = append(notFunctioningDomains, domain)
-					log.Printf("[%s] is [NOT FUNCTIONING].", domain)
+					notFunctioningList = append(notFunctioningList, domain)
+					log.Printf("[%s] is not functioning with error: %v.", domain, err)
 					return
 				} else if resp.StatusCode != http.StatusOK {
-					wrongcodeDomains = append(wrongcodeDomains, domain)
+					wrongCodeList[domain] = resp.StatusCode
 					log.Printf("[%s] is [RETURNING %d].", domain, resp.StatusCode)
 					return
 				}
 
-				// Mark as valid
-				validDomains = append(validDomains, domain)
-				log.Printf("[%s] is valid.", domain)
+				log.Printf("[%s] is valid, gathering nodeinfo.", domain)
+
+				// Bind nodeinfo list to json
+				var infoList NodeInfoList
+				err = json.NewDecoder(resp.Body).Decode(&infoList)
+				if err != nil {
+					misformattedNodeInfoListList = append(misformattedNodeInfoListList, domain)
+					log.Printf("[%s] 's nodeinfo list is invalid with error: %v.", domain, err)
+					return
+				}
+
+				// Get nodeinfo
+				nodeinfoSchemaLink := ""
+				for _, item := range infoList.Links {
+					if strings.HasPrefix(item.Rel, "http://nodeinfo.diaspora.software/ns/schema/2.") {
+						// Both 2.0 and 2.1 is fine
+						nodeinfoSchemaLink = item.Href
+						break
+					}
+				}
+
+				if nodeinfoSchemaLink == "" {
+					// No valid nodeinfo schema
+					noAvailableNodeInfoSchemaList = append(noAvailableNodeInfoSchemaList, domain)
+					log.Printf("[%s] doesn't have an available nodeinfo schema.", domain)
+					return
+				}
+
+				resp, err = http.Get(nodeinfoSchemaLink)
+				if err != nil {
+					noAvailableNodeInfoSchemaList = append(noAvailableNodeInfoSchemaList, domain)
+					log.Printf("[%s] 's schema href is not accessible with error: %v.", domain, err)
+					return
+				}
+
+				var infoSchema NodeInfoSchema
+				err = json.NewDecoder(resp.Body).Decode(&infoSchema)
+				if err != nil {
+					misformattedNodeInfoSchemaList = append(misformattedNodeInfoSchemaList, domain)
+					log.Printf("[%s] 's nodeinfo schema is invalid with error: %v.", domain, err)
+					return
+				}
+
+				// Finally this instance can be marked as valid
+				validList[domain] = infoSchema
 			}()
 		}
 	}
@@ -68,15 +125,99 @@ func main() {
 
 	log.Printf(
 		"Found %d unresolved, %d not functioning, %d wrong code, %d is still valid",
-		len(unresolvedDomains),
-		len(notFunctioningDomains),
-		len(wrongcodeDomains),
-		len(validDomains),
+		len(unresolvedList),
+		len(notFunctioningList),
+		len(wrongCodeList),
+		len(misformattedNodeInfoListList)+len(noAvailableNodeInfoSchemaList)+len(misformattedNodeInfoSchemaList)+len(validList),
 	)
 
-	os.WriteFile("unresolved.txt", []byte(strings.Join(unresolvedDomains, "\n")), 0644)
-	os.WriteFile("notfunctioning.txt", []byte(strings.Join(notFunctioningDomains, "\n")), 0644)
-	os.WriteFile("wrongcode.txt", []byte(strings.Join(wrongcodeDomains, "\n")), 0644)
-	os.WriteFile("valid.txt", []byte(strings.Join(validDomains, "\n")), 0644)
+	// Read last time result
+	var lastTimeResult ResultFileFormat
 
+	if _, err = os.Stat(ResultSaveFileName); err != nil {
+		log.Printf("Failed to check last time result file with error: %v.", err)
+	} else if lastTimeResultBytes, err := os.ReadFile(ResultSaveFileName); err != nil {
+		log.Printf("Failed to read last time result file with error: %v.", err)
+	} else if err = json.Unmarshal(lastTimeResultBytes, &lastTimeResult); err != nil {
+		log.Printf("Failed to unmarshal last time result file with error: %v.", err)
+	}
+
+	// Generate current result file (json)
+	currentTime := time.Now()
+	var result ResultFileFormat
+	result.CollectedAt = currentTime
+
+	result.Unresolved = InheritStatus(lastTimeResult.Unresolved, unresolvedList, currentTime)
+	result.NotFunctioning = InheritStatus(lastTimeResult.NotFunctioning, notFunctioningList, currentTime)
+	result.WrongCode = InheritStatusAndCode(lastTimeResult.WrongCode, wrongCodeList, currentTime)
+
+	result.MisformattedNodeInfoList = InheritStatus(lastTimeResult.MisformattedNodeInfoList, misformattedNodeInfoListList, currentTime)
+	result.NoAvailableNodeInfoSchema = InheritStatus(lastTimeResult.NoAvailableNodeInfoSchema, noAvailableNodeInfoSchemaList, currentTime)
+	result.MisformattedNodeInfoSchema = InheritStatus(lastTimeResult.MisformattedNodeInfoSchema, misformattedNodeInfoSchemaList, currentTime)
+
+	for domain, nodeinfo := range validList {
+		result.Valid = append(result.Valid, DomainValidWithNodeinfo{
+			Domain:   domain,
+			NodeInfo: nodeinfo,
+		})
+	}
+
+	resultBytes, err := json.Marshal(&result)
+	if err != nil {
+		log.Fatalf("Failed to format result into bytes with error: %v", err)
+		return
+	}
+
+	err = os.WriteFile(ResultSaveFileName, resultBytes, 0644)
+	if err != nil {
+		log.Fatalf("Failed to save result into file with error: %v", err)
+	}
+
+	log.Printf("%d domains validate finished successfully.", totalDomainCounter)
+
+}
+
+func InheritStatus(oldList []DomainErrorStatus, currentList []string, currentTime time.Time) []DomainErrorStatus {
+	var newList []DomainErrorStatus
+	for _, errDomain := range currentList {
+		dErr := DomainErrorStatus{
+			Domain: errDomain,
+			Since:  currentTime,
+		}
+		// Find if it's first time or has been a while
+		if len(oldList) > 0 {
+			for _, oldErrRecord := range oldList {
+				if oldErrRecord.Domain == errDomain {
+					dErr.Since = oldErrRecord.Since
+					break
+				}
+			}
+		}
+		newList = append(newList, dErr)
+	}
+	return newList
+}
+
+func InheritStatusAndCode(oldList []DomainErrorStatusWithCode, currentList map[string]int, currentTime time.Time) []DomainErrorStatusWithCode {
+	var newList []DomainErrorStatusWithCode
+	for errDomain, errCode := range currentList {
+		dErr := DomainErrorStatusWithCode{
+			DomainErrorStatus: DomainErrorStatus{
+				Domain: errDomain,
+				Since:  currentTime,
+			},
+			Code: errCode,
+		}
+		// Find if it's first time or has been a while
+		if len(oldList) > 0 {
+			for _, oldErrRecord := range oldList {
+				if oldErrRecord.Domain == errDomain && oldErrRecord.Code == errCode {
+					dErr.Since = oldErrRecord.Since
+					break
+				}
+			}
+		}
+		newList = append(newList, dErr)
+	}
+	return newList
 }
---
+++ types.go
@@ -0,0 +1,66 @@
+package main
+
+import "time"
+
+// NodeInfoSchema : Refer to https://nodeinfo.diaspora.software/schema.html
+type NodeInfoSchema struct {
+	Version  string `json:"version"`
+	Software struct {
+		Name       string  `json:"name"`
+		Version    string  `json:"version"`
+		Repository *string `json:"repository,omitempty"`
+	} `json:"software"`
+	Protocols         []string `json:"protocols"`
+	Services          any      `json:"services"`
+	OpenRegistrations bool     `json:"openRegistrations"`
+	Usage             struct {
+		Users struct {
+			Total          uint64  `json:"total"`
+			ActiveHalfyear *uint64 `json:"activeHalfyear"`
+			ActiveMonth    *uint64 `json:"activeMonth"`
+		} `json:"users"`
+		LocalPosts    *uint64 `json:"localPosts"`
+		LocalComments *uint64 `json:"localComments"`
+	} `json:"usage"`
+	Metadata any `json:"metadata"`
+}
+
+type NodeInfoList struct {
+	Links []struct {
+		Rel  string `json:"rel"` // http://nodeinfo.diaspora.software/ns/schema/2.1
+		Href string `json:"href"`
+	} `json:"links"`
+}
+
+type DomainErrorStatus struct {
+	Domain string    `json:"domain"`
+	Since  time.Time `json:"since"`
+}
+
+type DomainErrorStatusWithCode struct {
+	DomainErrorStatus
+
+	Code int `json:"code"`
+}
+
+type DomainValidWithNodeinfo struct {
+	Domain   string         `json:"domain"`
+	NodeInfo NodeInfoSchema `json:"nodeinfo"`
+}
+
+type ResultFileFormat struct {
+	CollectedAt time.Time `json:"collected_at"`
+
+	// Not working
+	Unresolved     []DomainErrorStatus         `json:"unresolved"`
+	NotFunctioning []DomainErrorStatus         `json:"not_functioning"`
+	WrongCode      []DomainErrorStatusWithCode `json:"wrong_code"`
+
+	// Working but failed to get node info
+	MisformattedNodeInfoList   []DomainErrorStatus `json:"misformatted_nodeinfo_list"`
+	NoAvailableNodeInfoSchema  []DomainErrorStatus `json:"no_available_nodeinfo_schema"`
+	MisformattedNodeInfoSchema []DomainErrorStatus `json:"misformatted_nodeinfo_schema"`
+
+	// Fully functional
+	Valid []DomainValidWithNodeinfo `json:"valid"`
+}
