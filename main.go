fc2d4dd2e58788a5e4c547a1ffed5043517b505f
Nya Candy <dev@candinya.com>
2023-10-28 12:16:10 +0800

    fix: use channel to prevent async write conflict

--- main.go
+++ main.go
@@ -19,121 +19,129 @@ const (
 )
 
 func main() {
-	domains, err := os.ReadFile(RelayListDomainsFileName)
+	relayOutputResult, err := os.ReadFile(RelayListDomainsFileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fileLines := strings.Split(strings.ReplaceAll(string(domains), "\r", ""), "\n")
+	fileLines := strings.Split(strings.ReplaceAll(string(relayOutputResult), "\r", ""), "\n")
 
-	totalDomainCounter := 0
+	var allDomains []string
 
-	var unresolvedList []string
-	var notFunctioningList []string
-	var wrongCodeList []domainWithErrorCode
-
-	var misformattedNodeInfoListList []string
-	var noAvailableNodeInfoSchemaList []string
-	var misformattedNodeInfoSchemaList []string
-	var validList []domainWithValidNodeinfo
-
-	var wg sync.WaitGroup
-
+	// Collect all domains list
 	for _, line := range fileLines {
-
 		if strings.Contains(line, "[*]") {
 			// Is domain
-			wg.Add(1)
-
-			totalDomainCounter++
-
-			domain := strings.Replace(line, "[*] ", "", 1)
-
-			go func() {
-				defer wg.Done()
-
-				// Try resolve
-				_, err := net.LookupIP(strings.Split(domain, ":")[0])
-				if err != nil {
-					unresolvedList = append(unresolvedList, domain)
-					log.Printf("[%s] is unresolved.", domain)
-					return
-				}
-
-				// Try get nodeinfo
-				wellknownNodeinfo := fmt.Sprintf("https://%s/.well-known/nodeinfo", domain)
-
-				resp, err := (&http.Client{
-					Timeout: RequestTimeout,
-				}).Get(wellknownNodeinfo)
-				if err != nil {
-					notFunctioningList = append(notFunctioningList, domain)
-					log.Printf("[%s] is not functioning with error: %v.", domain, err)
-					return
-				} else if resp.StatusCode != http.StatusOK {
-					wrongCodeList = append(wrongCodeList, domainWithErrorCode{
-						Domain: domain,
-						Code:   resp.StatusCode,
-					})
-					log.Printf("[%s] is [RETURNING %d].", domain, resp.StatusCode)
-					return
-				}
-
-				log.Printf("[%s] seems to be valid, gathering nodeinfo.", domain)
-
-				// Bind nodeinfo list to json
-				var infoList NodeInfoList
-				err = json.NewDecoder(resp.Body).Decode(&infoList)
-				if err != nil {
-					misformattedNodeInfoListList = append(misformattedNodeInfoListList, domain)
-					log.Printf("[%s] 's nodeinfo list is invalid with error: %v.", domain, err)
-					return
-				}
-
-				// Get nodeinfo
-				nodeinfoSchemaLink := ""
-				for _, item := range infoList.Links {
-					if strings.HasPrefix(item.Rel, "http://nodeinfo.diaspora.software/ns/schema/2.") {
-						// Both 2.0 and 2.1 is fine
-						nodeinfoSchemaLink = item.Href
-						break
-					}
-				}
-
-				if nodeinfoSchemaLink == "" {
-					// No valid nodeinfo schema
-					noAvailableNodeInfoSchemaList = append(noAvailableNodeInfoSchemaList, domain)
-					log.Printf("[%s] doesn't have an available nodeinfo schema.", domain)
-					return
-				}
-
-				resp, err = (&http.Client{
-					Timeout: RequestTimeout,
-				}).Get(nodeinfoSchemaLink)
-				if err != nil {
-					noAvailableNodeInfoSchemaList = append(noAvailableNodeInfoSchemaList, domain)
-					log.Printf("[%s] 's schema href is not accessible with error: %v.", domain, err)
-					return
-				}
-
-				var infoSchema NodeInfoSchema
-				err = json.NewDecoder(resp.Body).Decode(&infoSchema)
-				if err != nil {
-					misformattedNodeInfoSchemaList = append(misformattedNodeInfoSchemaList, domain)
-					log.Printf("[%s] 's nodeinfo schema is invalid with error: %v.", domain, err)
-					return
-				}
-
-				// Finally this instance can be marked as valid
-				validList = append(validList, domainWithValidNodeinfo{
-					Domain:   domain,
-					NodeInfo: infoSchema,
-				})
-			}()
+			allDomains = append(allDomains, strings.Replace(line, "[*] ", "", 1))
 		}
 	}
 
+	// Use channel to prevent async write conflict
+	unresolvedList := make(chan string, len(allDomains))
+	notFunctioningList := make(chan string, len(allDomains))
+	wrongCodeList := make(chan domainWithErrorCode, len(allDomains))
+	misformattedNodeInfoListList := make(chan string, len(allDomains))
+	noAvailableNodeInfoSchemaList := make(chan string, len(allDomains))
+	misformattedNodeInfoSchemaList := make(chan string, len(allDomains))
+	validList := make(chan domainWithValidNodeinfo, len(allDomains))
+
+	var wg sync.WaitGroup
+	for _, domain := range allDomains {
+		wg.Add(1)
+		domain := domain
+		go func() {
+			defer wg.Done()
+
+			// Try resolve
+			_, err := net.LookupIP(strings.Split(domain, ":")[0])
+			if err != nil {
+				unresolvedList <- domain
+				log.Printf("[%s] is unresolved.", domain)
+				return
+			}
+
+			// Try get nodeinfo
+			wellknownNodeinfo := fmt.Sprintf("https://%s/.well-known/nodeinfo", domain)
+
+			resp, err := (&http.Client{
+				Timeout: RequestTimeout,
+			}).Get(wellknownNodeinfo)
+			if err != nil {
+				notFunctioningList <- domain
+				log.Printf("[%s] is not functioning with error: %v.", domain, err)
+				return
+			} else if resp.StatusCode != http.StatusOK {
+				wrongCodeList <- domainWithErrorCode{
+					Domain: domain,
+					Code:   resp.StatusCode,
+				}
+				log.Printf("[%s] is [RETURNING %d].", domain, resp.StatusCode)
+				return
+			}
+
+			log.Printf("[%s] seems to be valid, gathering nodeinfo.", domain)
+
+			// Bind nodeinfo list to json
+			var infoList NodeInfoList
+			err = json.NewDecoder(resp.Body).Decode(&infoList)
+			if err != nil {
+				misformattedNodeInfoListList <- domain
+				log.Printf("[%s] 's nodeinfo list is invalid with error: %v.", domain, err)
+				return
+			}
+
+			// Get nodeinfo
+			nodeinfoSchemaLink := ""
+			for _, item := range infoList.Links {
+				if strings.HasPrefix(item.Rel, "http://nodeinfo.diaspora.software/ns/schema/2.") {
+					// Both 2.0 and 2.1 is fine
+					nodeinfoSchemaLink = item.Href
+					break
+				}
+			}
+
+			if nodeinfoSchemaLink == "" {
+				// No valid nodeinfo schema
+				noAvailableNodeInfoSchemaList <- domain
+				log.Printf("[%s] doesn't have an available nodeinfo schema.", domain)
+				return
+			}
+
+			resp, err = (&http.Client{
+				Timeout: RequestTimeout,
+			}).Get(nodeinfoSchemaLink)
+			if err != nil {
+				noAvailableNodeInfoSchemaList <- domain
+				log.Printf("[%s] 's schema href is not accessible with error: %v.", domain, err)
+				return
+			}
+
+			var infoSchema NodeInfoSchema
+			err = json.NewDecoder(resp.Body).Decode(&infoSchema)
+			if err != nil {
+				misformattedNodeInfoSchemaList <- domain
+				log.Printf("[%s] 's nodeinfo schema is invalid with error: %v.", domain, err)
+				return
+			}
+
+			// Finally this instance can be marked as valid
+			validList <- domainWithValidNodeinfo{
+				Domain:   domain,
+				NodeInfo: infoSchema,
+			}
+		}()
+	}
+
 	wg.Wait()
+
+	// Finish collect
+	close(unresolvedList)
+	close(notFunctioningList)
+	close(wrongCodeList)
+	close(misformattedNodeInfoListList)
+	close(noAvailableNodeInfoSchemaList)
+	close(misformattedNodeInfoSchemaList)
+	close(validList)
 
 	log.Printf(
 		"Found %d unresolved, %d not functioning, %d wrong code, %d + %d + %d unknown, %d is still valid",
@@ -169,7 +177,7 @@ func main() {
 	result.MisformattedNodeInfoSchema = InheritStatus(lastTimeResult.MisformattedNodeInfoSchema, misformattedNodeInfoSchemaList, currentTime)
 
 	validResults := make(ResultValidWithNodeInfo)
-	for _, validDomain := range validList {
+	for validDomain := range validList {
 		validResults[validDomain.Domain] = validDomain.NodeInfo
 	}
 	result.Valid = validResults
@@ -185,13 +193,13 @@ func main() {
 		log.Fatalf("Failed to save result into file with error: %v", err)
 	}
 
-	log.Printf("%d domains validate finished successfully.", totalDomainCounter)
+	log.Printf("%d domains validate finished successfully.", len(allDomains))
 
 }
 
-func InheritStatus(oldList ResultErrRecord, currentList []string, currentTime time.Time) ResultErrRecord {
+func InheritStatus(oldList ResultErrRecord, currentList chan string, currentTime time.Time) ResultErrRecord {
 	newList := make(ResultErrRecord)
-	for _, errDomain := range currentList {
+	for errDomain := range currentList {
 		dErr := currentTime
 		// Find if it's first time or has been a while
 		if len(oldList) > 0 {
@@ -207,9 +215,9 @@ func InheritStatus(oldList ResultErrRecord, currentList []string, currentTime ti
 	return newList
 }
 
-func InheritStatusAndCode(oldList ResultErrRecordWithCode, currentList []domainWithErrorCode, currentTime time.Time) ResultErrRecordWithCode {
+func InheritStatusAndCode(oldList ResultErrRecordWithCode, currentList chan domainWithErrorCode, currentTime time.Time) ResultErrRecordWithCode {
 	newList := make(ResultErrRecordWithCode)
-	for _, errDomain := range currentList {
+	for errDomain := range currentList {
 		dErr := ErrorStatusWithCode{
 			Code:  errDomain.Code,
 			Since: currentTime,
